4fde8b5ccb67847878c193d71c2044f470aa9433
crtepitanga <146767040+crtepitanga@users.noreply.github.com>
2024-03-05 08:29:56 -0300

    Update p2-d480.sh

--- p2-d480.sh
+++ p2-d480.sh
@@ -262,6 +262,18 @@ bloquearaplicativos(){
    done
   
 }
+modokiosk(){
+	cd /tmp
+	
+		if [ -e k.sh ];then
+	 		bash k.sh
+		else  	
+			wget https://raw.githubusercontent.com/crtepitanga/scripts/main/k.sh -O k.sh
+			bash k.sh
+		
+		fi
+}
+
 
 PREFIXO='e'
 TIPO=\$( dmidecode -t system | grep 'Product Name: ' | cut -d':' -f2 | sed -e s/'^ '// -e s/' '/'_'/g )
@@ -293,6 +305,7 @@ POS-EIB85CZ)
     limparguests
     instalascratch
     bloquearaplicativos
+    modokiosk
   ;;  
   Positivo_Duo_ZE3630)
     exit
