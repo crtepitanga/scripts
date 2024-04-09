2aa57ff3b805d166939defed1d09ca648e8325b6
crtepitanga <146767040+crtepitanga@users.noreply.github.com>
2024-04-09 08:27:40 -0300

    Add files via upload
    Script para desbloquear apps

---
+++ desbloquearapps.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#-----------------------------------------Desativar acesso aos usuarios aos aplicativos desejados-----------------------------------#
+
+# Desativar acesso aos usuarios nos aplicativos a seguir:
+USUARIOS=( "pedagogico" "professor" "escola" "Aluno" "aluno" "alunos" )
+APLICATIVOS=( "/usr/bin/xfce4-appearance-settings" "/usr/bin/users-admin" "/usr/bin/mugshot" "/usr/bin/mate-about-me" "/usr/bin/mintupdate" "/usr/bin/mintinstall" "/usr/bin/nm-connection/editor" )
+
+for USUARIO in "${USUARIOS[@]}" ; do 
+   if [ $(grep "^${USUARIO}:" /etc/passwd | wc -l) -eq 0 ]; then
+      echo "Usuario $USUARIO nao consta neste Linux"
+      continue
+   fi
+   for APLICATIVO in "${APLICATIVOS[@]}" ; do
+      if [ -x "$APLICATIVO" ]; then
+         /bin/setfacl -b "$APLICATIVO"
+      fi
+   done
+   echo "Aplicativos alterar usuario e senha desbloqueados para $USUARIO"
+echo "Aplicativo Aparencia desbloqueado para $USUARIO"
+done
+
+#-----------------------------------------Fim Desativar acesso aos usuarios aos aplicativos desejados-----------------------------------#
+
+
+#---------------Habilitar arquivo para descanso de tela na tela de login---------------#
+#/etc/init.d/lightdm restart
+
+#reboot
