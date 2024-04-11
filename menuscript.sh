13616dd0cc5e2af28c9cf1c574436daeafc5b033
crtepitanga <146767040+crtepitanga@users.noreply.github.com>
2024-04-11 14:47:09 -0300

    Update menuscript.sh

--- menuscript.sh
+++ menuscript.sh
@@ -1318,7 +1318,28 @@ function bloquearaplicativos(){
         echo "BLOQUEADO APPS E AREA DE TRABALHO👍️👍️👍️"
     fi
 }
+function desbloquearaplicativos(){
+#-----------------------------------------Ativar acesso aos usuarios aos aplicativos desejados-----------------------------------#
 
+USUARIOS=( "pedagogico" "professor" "escola" "Aluno" "aluno" "alunos" )
+APLICATIVOS=( "/usr/bin/xfce4-appearance-settings" "/usr/bin/users-admin" "/usr/bin/mugshot" "/usr/bin/mate-about-me" "/usr/bin/mintupdate" "/usr/bin/mintinstall" "/usr/bin/nm-connection/editor" )
+
+for USUARIO in "${USUARIOS[@]}" ; do 
+   if [ $(grep "^${USUARIO}:" /etc/passwd | wc -l) -eq 0 ]; then
+      echo "Usuario $USUARIO nao consta neste Linux"
+      continue
+   fi
+   for APLICATIVO in "${APLICATIVOS[@]}" ; do
+      if [ -x "$APLICATIVO" ]; then
+         /bin/setfacl -b "$APLICATIVO"
+      fi
+   done
+   echo "Aplicativos alterar usuario e senha desbloqueados para $USUARIO"
+echo "Aplicativo Aparencia desbloqueado para $USUARIO"
+done
+
+#-----------------------------------------Fim Desativar acesso aos usuarios aos aplicativos desejados-----------------------------------#
+}
 function atualizahoraedata(){
 cd /tmp
     if [ -e ah.sh ]; then
