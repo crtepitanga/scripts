7758518ae82111fc31c434ecb64da286cbe39ad9
crtepitanga <146767040+crtepitanga@users.noreply.github.com>
2024-04-18 16:56:34 -0300

    Update p2-completo.sh

--- p2-completo.sh
+++ p2-completo.sh
@@ -442,8 +442,39 @@ case "\$TIPO" in
   
   ;;  
   Positivo_Duo_ZE3630)
-   
+    PREFIXO='n'
+    echo -e "\e[46mNetbook Verde Linux Mint \e[0m "
+    echo "########################################################################"
+   	 echo -e "\e[46mInserindo atalho Página Inicial na Área de Trabalho  \e[0m "
+    atalhopaginainicial
+     echo "########################################################################"
+    	echo -e "\e[46mInstalando Modo Kiosk\e[0m "
+    modokiosk   
+     echo "########################################################################"
+    	echo -e "\e[46mDesabilitando Bloqueio de Tela \e[0m "
+    tirarbloqueiodetela
+     echo "########################################################################"
+   	 echo -e "\e[46mAtivando Plano de Fundo Padrão\e[0m "
+    ativartrocafundodetela
+     echo "########################################################################"
+    	echo -e "\e[46mResetando Plano de Fundo \e[0m "
+    resetbackgrounds    
+     echo "########################################################################"
+    	echo -e "\e[46mLimpando usuários Guest \e[0m "
+    limparguests
+     echo "########################################################################"
+    	echo -e "\e[46mInstalando Scratch \e[0m "
+    instalascratch   
+     echo "########################################################################"
+    rodarEmNetVerde
+    	echo -e "\e[46mAtualizando os Navegadores\e[0m "
+    atualizaNavegadoresAtomVscodeEtc
+     echo "########################################################################"
+	echo -e "\e[46mBloqueando Aplicativos e Área de Trabalho \e[0m "
+    bloquearaplicativos
+     echo "########################################################################"
   ;;
+  
  N4340)
     PREFIXO='n'
     echo "Notebook Integral encontrado aqui"
