2149820b9607d62f26b1b11ccd67bb726821cd75
crtepitanga <146767040+crtepitanga@users.noreply.github.com>
2024-04-19 10:40:37 -0300

    Update p2-verdinhos.sh

--- p2-verdinhos.sh
+++ p2-verdinhos.sh
@@ -211,22 +211,23 @@ limparguests() {
 }
 
 ativartrocafundodetela(){
-  cd /tmp
+    cd /tmp
     if [ -e atf1.sh ]; then
-        #rm atf1.sh
-        echo "Jah tinha atf1.sh no temp"
+        rm atf1.sh
+	wget jonilso.com/atf1.sh
+        bash  atf1.sh
     else
         wget jonilso.com/atf1.sh
         bash  atf1.sh
     fi
- 
-   
+
 }
 atalhopaginainicial(){
     cd /tmp
     if [ -e atalhopaginainicial.sh ]; then
-        #rm atalhopaginainicial.sh
-        echo "Jah tinha atalhopaginainicial.sh no temp"
+        rm atalhopaginainicial.sh
+        wget jonilso.com/atalhopaginainicial.sh
+        bash  atalhopaginainicial.sh
     else
         wget jonilso.com/atalhopaginainicial.sh
         bash  atalhopaginainicial.sh
@@ -236,8 +237,9 @@ atalhopaginainicial(){
 instalascratch(){
     cd /tmp
     if [ -e s.sh ]; then
-        #rm s.sh
-        echo "Jah tinha s.sh no temp"
+        rm s.sh
+         wget -q jonilso.com/s.sh
+        bash  s.sh
     else
         wget -q jonilso.com/s.sh
         bash  s.sh
@@ -258,6 +260,7 @@ bloquearaplicativos(){
         echo "BLOQUEADO APPS E AREA DE TRABALHO👍️👍️👍️"
     fi
 }
+
 modokiosk(){
 	cd /tmp
 	
