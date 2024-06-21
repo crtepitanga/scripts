dbba03db403dd2cb9738348b3a9de6b2ac9327a7
crtepitanga <146767040+crtepitanga@users.noreply.github.com>
2024-06-21 17:24:06 -0300

    Update fn.sh

--- fn.sh
+++ fn.sh
@@ -13,32 +13,47 @@ echo "Iniciando, aguarde ..."
 # Desabiliar a função fn do teclado do Netbook
 ################################################################################
  
- echo -e "\e[43m#################### DESABILITAR A FUNÇÃO FN DO TECLADO DO NETBOOK ###################### \e[0m "
+echo -e "\e[43m#################### DESABILITAR A FUNÇÃO FN DO TECLADO DO NETBOOK ###################### \e[0m "
+
+if [ -e /home/administrador/.config/xfce4/xfconf/xfce-perchannel-xml ]; then
    cd /home/administrador/.config/xfce4/xfconf/xfce-perchannel-xml && sed -i 's/value="true"/value="false"/' keyboards.xml
    cat keyboards.xml
    echo -e "\e[46m============ Desabilitado a função fn do teclado do usuário administrador ================= \e[0m "
+fi
 
+if [ -e /home/escola/.config/xfce4/xfconf/xfce-perchannel-xml ]; then
    cd /home/escola/.config/xfce4/xfconf/xfce-perchannel-xml && sed -i 's/value="true"/value="false"/' keyboards.xml
    cat keyboards.xml
    echo -e "\e[46m============ Desabilitado a função fn do teclado do usuário escola ================= \e[0m "
+fi
+
+if [ -e /home/alunos/.config/xfce4/xfconf/xfce-perchannel-xml ]; then
    cd /home/alunos/.config/xfce4/xfconf/xfce-perchannel-xml && sed -i 's/value="true"/value="false"/' keyboards.xml
    cat keyboards.xml
    echo -e "\e[46m============ Desabilitado a função fn do teclado do usuário alunos ================= \e[0m "
+fi
+
+if [ -e /home/pedagogico/.config/xfce4/xfconf/xfce-perchannel-xml ]; then
    cd /home/pedagogico/.config/xfce4/xfconf/xfce-perchannel-xml && sed -i 's/value="true"/value="false"/' keyboards.xml
    cat keyboards.xml
    echo -e "\e[46m============ Desabilitado a função fn do teclado do usuário pedagogico ================= \e[0m "
+fi
+
+
+if [ -e /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml ]; then
    cd /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml && sed -i 's/value="true"/value="false"/' keyboards.xml
    cat keyboards.xml
    echo -e "\e[46m============ Desabilitado a função fn do teclado do usuário convidado ================= \e[0m "oo
-   echo ""
-   echo ""
+fi
+
+echo ""
+echo ""
    
  echo -e "\e[43m################## FIM DESABILITAR A FUNÇÃO FN DO TECLADO DO NETBOOK ####################### \e[0m "
  
    
    
 
-   #cat keyboards.xml
 
 
 
