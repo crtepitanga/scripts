cf44bddb9ec587473ae03552864a1241a409a9df
crtepitanga <146767040+crtepitanga@users.noreply.github.com>
2024-07-02 17:07:22 -0300

    Update p2-dell.sh

--- p2-dell.sh
+++ p2-dell.sh
@@ -674,5 +674,5 @@ echo -e "\e[46m============ Script menuv01 excluido, favor digitar o comando wge
 fi
 
 # ----REMOVER SCRIPT p2-dell.sh FINALIZAR------------
-cd /tmp && rm p2-dell.sh
+cd /tmp && rm *.sh
 
