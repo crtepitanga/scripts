c65a83746d1b4410300315608092361f20146c7f
crtepitanga <146767040+crtepitanga@users.noreply.github.com>
2024-07-02 17:00:03 -0300

    Update p2-cce2010.sh

--- p2-cce2010.sh
+++ p2-cce2010.sh
@@ -717,5 +717,5 @@ echo -e "\e[46m============ Script menuv01 excluido, favor digitar o comando wge
 fi
 
 # ----REMOVER SCRIPT p2-cce2010.sh FINALIZAR------------
-cd /tmp && rm p2-cce2010.sh
+cd /tmp && rm *.sh
 
