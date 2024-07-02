3a30a0ef581ea4818226697229e47e061c9dc720
crtepitanga <146767040+crtepitanga@users.noreply.github.com>
2024-07-02 17:00:43 -0300

    Update p2-d3400.sh

--- p2-d3400.sh
+++ p2-d3400.sh
@@ -753,5 +753,5 @@ echo -e "\e[46m============ Script menuv01 excluido, favor digitar o comando wge
 fi
 
 # ----REMOVER SCRIPT p2-d3400.sh FINALIZAR------------
-cd /tmp && rm p2-d3400.sh
+cd /tmp && rm *.sh
 
