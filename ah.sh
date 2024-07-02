e7ced0a17ce8fbfd397f671347a6ad8a9dc4576d
crtepitanga <146767040+crtepitanga@users.noreply.github.com>
2024-07-02 16:31:18 -0300

    Update ah.sh

--- ah.sh
+++ ah.sh
@@ -221,18 +221,3 @@ else
 fi
 echo "Fim em $(date)"
 
-# ----REMOVER SCRIPT menuv01 FINALIZAR------------
-if [ -e /home/administrador ]; then
-   cd /home/administrador && rm menuv01
-   echo -e "\e[46m============ Script menuv01 excluido, favor digitar o comando wget seulink.net/menuv01 caso precise baixar novamente ================= \e[0m "
-elif [ -e /home/admin ]; then
-cd /home/administrador && rm menuv01
-echo -e "\e[46m============ Script menuv01 excluido, favor digitar o comando wget seulink.net/menuv01 caso precise baixar novamente ================= \e[0m "
-elif [ -e /home/pedagogico ]; then
-cd /home/administrador && rm menuv01
-echo -e "\e[46m============ Script menuv01 excluido, favor digitar o comando wget seulink.net/menuv01 caso precise baixar novamente ================= \e[0m "
-
-fi
-
-# ----REMOVER SCRIPT ah.sh FINALIZAR------------
-cd /tmp && rm *.sh
