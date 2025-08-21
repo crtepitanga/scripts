abd87346c8f9e904456ad3008c7ebc8c2d4f6b35
crtepitanga <146767040+crtepitanga@users.noreply.github.com>
2025-08-21 16:03:41 -0300

    Update bloquearapps.sh

--- bloquearapps.sh
+++ bloquearapps.sh
@@ -40,6 +40,8 @@ for usuario in *; do
        sudo cp /usr/share/applications/gns3.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
        sudo cp /usr/share/applications/mysql-workbench.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
        sudo cp /usr/share/applications/scratch-desktop.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
+       sudo cp /usr/share/applications/arduino232.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
+       sudo cp $usuario/Área  arduino232.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
 
        sudo cp /usr/share/applications/gimp.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
        sudo cp /usr/share/applications/orange.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
@@ -76,6 +78,7 @@ for usuario in *; do
        sudo cp /usr/share/applications/gns3.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
        sudo cp /usr/share/applications/mysql-workbench.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
        sudo cp /usr/share/applications/scratch-desktop.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
+       sudo cp /usr/share/applications/arduino232.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
 
        sudo cp /usr/share/applications/gimp.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
        sudo cp /usr/share/applications/orange.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
