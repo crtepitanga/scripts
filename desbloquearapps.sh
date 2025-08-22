009b6bd2d6a00f23d425b15a667cc416d01520e0
crtepitanga <146767040+crtepitanga@users.noreply.github.com>
2025-08-22 11:24:11 -0300

    Update desbloquearapps.sh

--- desbloquearapps.sh
+++ desbloquearapps.sh
@@ -40,6 +40,8 @@ for usuario in *; do
 
        sudo cp /usr/share/applications/gimp.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
        sudo cp /usr/share/applications/orange.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null 
+       sudo cp /usr/share/applications/arduino232.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
+       sudo cp /usr/share/applications/mblock.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
       
        sudo chown $usuario.$usuario -R /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
        sudo chown -h $usuario /home/$usuario/Área\ de\ trabalho/*.desktop 1>/dev/null 2>/dev/null
@@ -74,6 +76,9 @@ for usuario in *; do
 
        sudo cp /usr/share/applications/gimp.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
        sudo cp /usr/share/applications/orange.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
+       sudo cp /usr/share/applications/arduino232.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
+       sudo cp /usr/share/applications/mblock.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
+       
        sudo cp -r /home/administrador/Downloads/paginainicial.desktop /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
        sudo chown root.root -R /home/$usuario/Área\ de\ trabalho/ 1>/dev/null 2>/dev/null
        sudo chown -h $usuario /home/$usuario/Área\ de\ trabalho/*.desktop 1>/dev/null 2>/dev/null
