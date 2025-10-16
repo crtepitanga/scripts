3ae9ce46461a032175cdacefa9f46ff236771454
crtepitanga <146767040+crtepitanga@users.noreply.github.com>
2025-10-16 11:33:12 -0300

    Update p2-ST4266.sh

--- p2-ST4266.sh
+++ p2-ST4266.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+F#!/bin/bash
 
 # Script com atualização dos navegadores, com instalação do av.sh e que funcione para PC com usuario
 
@@ -118,10 +118,18 @@ atualizaNavegadoresAtomVscodeEtc() {
       cd /tmp
       wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
       rm repositorios.deb 2>> /dev/null
-      wget http://ubuntu.celepar.parana/repositorios.deb
-      if [ -e repositorios.deb ]; then
-          dpkg -i repositorios.deb
-      fi
+	  
+      #wget http://ubuntu.celepar.parana/repositorios.deb
+      #if [ -e repositorios.deb ]; then
+          #dpkg -i repositorios.deb
+      #fi
+	rm rs.sh 
+	wget https://raw.githubusercontent.com/crtepitanga/scripts/refs/heads/main/rs.sh -O rs.sh
+	if [ -e rs.sh ];then
+		bash rs.sh
+		
+	fi
+	  
       apt-get  update
       apt-get -y install firefox firefox-locale-pt google-chrome-stable
       rm pi1.sh 2>> /dev/null
